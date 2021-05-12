4d95b850e3cae445fa022abc1d811c54ae5b9cb5
colamong <legoddsl@gmail.com>
2021-05-12 00:21:04 -0700

    Add some files

---
+++ hello.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+#inlcude "myheader.h"
+
+void printHello(void) {printf("Hello World!\n");}
+
---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "myheader.h"
+
+in main(void){
+	printHello();
+	printBye();
+
+	return 0;
+}
